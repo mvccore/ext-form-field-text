8f0ab6c672486b9eab1f9ef7817362a701c5b639
Tom Flídr <tomflidr@gmail.com>
2021-04-28 23:07:12 +0200

    release 5.1.3

--- src/MvcCore/Ext/Forms/Fields/Text.php
+++ src/MvcCore/Ext/Forms/Fields/Text.php
@@ -50,7 +50,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.2';
+	const VERSION = '5.1.3';
 
 	/**
 	 * Possible values: `text` and `email`, `password`, `search`, `tel` and `url` in extended class.
